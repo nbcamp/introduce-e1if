4052add715c9cd9b7837cfb532d1090e7fa2f2da
sanc93 <sanc93@me.com>
2023-07-12 20:05:15 +0900

    댓글 기능 (23/7/12 20:00)

--- lib/screens/sanghun.dart
+++ lib/screens/sanghun.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors, must_be_immutable
 import 'package:flutter/material.dart';
 
 class SanghunScreen extends StatefulWidget {
@@ -8,6 +9,19 @@ class SanghunScreen extends StatefulWidget {
 }
 
 class _SanghunScreenState extends State<SanghunScreen> {
+  // FocusNode textFocus = FocusNode(); // TODO : 다른곳 터치하면 텍스트필드 포커스 비활성화되도록..
+  TextEditingController commentController =
+      TextEditingController(); // TextField에 입력된 값을 가지고 오는 클래스
+
+  List<Comment> comments = [
+    Comment(id: '1', content: '테스트1', dateTime: DateTime.now()),
+    Comment(id: '2', content: '테[스트테스트테스트테스트', dateTime: DateTime.now()),
+    Comment(id: '3', content: 'ㅁㄴㅇㅁㄴㅇㄹㅁㄴㅇㅁㄴㅇㅁㄴㅇ', dateTime: DateTime.now()),
+  ];
+  String formatDate(DateTime dateTime) {
+    return '${dateTime.year}/${dateTime.month}/${dateTime.day} ${dateTime.hour}:${dateTime.minute}'; // dateTime 포맷 가공
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +39,7 @@ class _SanghunScreenState extends State<SanghunScreen> {
       ),
       body: SafeArea(
         child: ListView(
-          physics: const NeverScrollableScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
           padding: const EdgeInsets.all(30.0),
           children: [
             Row(
@@ -66,8 +80,8 @@ class _SanghunScreenState extends State<SanghunScreen> {
             const SizedBox(height: 30),
             ListView(
               shrinkWrap: true, // ListView가 필요한 공간만 차지하도록
-              physics: const NeverScrollableScrollPhysics(), // 스크롤 막기
-              children: const [
+              physics: const ScrollPhysics(),
+              children: [
                 Text(
                   ' 특기',
                 ),
@@ -92,17 +106,121 @@ class _SanghunScreenState extends State<SanghunScreen> {
                 ),
                 SizedBox(height: 30),
                 Divider(),
+                SizedBox(height: 10),
                 Flexible(
-                  child: TextField(
-                    // onSubmitted: (value) {
-                    // },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      // TODO: 댓글 기능
-                      labelText: '댓글',
-                    ),
+                  // TODO: 댓글 기능
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 40.0,
+                          child: TextField(
+                            controller: commentController,
+                            style: TextStyle(
+                                fontSize: 13.0,
+                                height: 1.2,
+                                color: Colors.black),
+                            onSubmitted: (query) {
+                              setState(() {
+                                comments = [
+                                  Comment(
+                                    id: DateTime.now().toString(),
+                                    content: query,
+                                    dateTime: DateTime.now(),
+                                  ),
+                                  ...comments,
+                                ];
+                              });
+                            },
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 15),
+                              hintText: '댓글을 입력하세요',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              fillColor: Colors.black12,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Colors.black87, width: 1.0),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 2),
+                      ElevatedButton(
+                        onPressed: () {
+                          String comment = commentController.text.trim();
+                          if (comment.isNotEmpty) {
+                            setState(() {
+                              comments.insert(
+                                0,
+                                Comment(
+                                  id: DateTime.now().toString(),
+                                  content: comment,
+                                  dateTime: DateTime.now(),
+                                ),
+                              );
+                            });
+                            commentController.clear();
+                          }
+                          print('test1111');
+                        },
+                        child: Icon(
+                          Icons.keyboard_return_rounded,
+                          size: 18,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black38,
+                          shape: CircleBorder(),
+                        ),
+                      )
+                    ],
                   ),
                 ),
+                SizedBox(height: 10),
+                if (comments.isEmpty)
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text('아직 댓글이 없습니다', textAlign: TextAlign.center),
+                  ),
+                ...comments.map((comment) => ListTile(
+                      title: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              comment.content,
+                              overflow: TextOverflow
+                                  .visible, // 텍스트가 너무 길어도 생략하지 않고 모두 표시
+                              softWrap: true, // 텍스트가 화면을 넘어갈 경우 자동으로 줄바꿈
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            formatDate(comment.dateTime),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.cancel,
+                          color: Colors.black26,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            comments = comments
+                                .where((c) => c.id != comment.id)
+                                .toList();
+                          });
+                        },
+                      ),
+                    ))
               ],
             ),
           ],
@@ -110,6 +228,18 @@ class _SanghunScreenState extends State<SanghunScreen> {
       ),
     );
   }
+}
+
+class Comment {
+  String id;
+  String content;
+  DateTime dateTime;
+
+  Comment({
+    required this.id,
+    required this.content,
+    required this.dateTime,
+  });
 }
 
 class TileBox extends StatelessWidget {
