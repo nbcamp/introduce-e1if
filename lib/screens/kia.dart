3e8ac5d84a7e72406515c9f8bb1960a00e4ae8b4
kiakim01 <hnmpot0318@naver.com>
2023-07-13 15:12:11 +0900

    코드 리팩토링

--- lib/screens/kia.dart
+++ lib/screens/kia.dart
@@ -6,6 +6,7 @@ import '../services/comment.dart';
 
 class KiaPage extends StatefulWidget {
   const KiaPage({Key? key}) : super(key: key);
+
   @override
   _KiaPageState createState() => _KiaPageState();
 }
@@ -16,6 +17,77 @@ class _KiaPageState extends State<KiaPage> {
     Comment(id: '2', author: '익명이', content: '강아지 너모귀여워요!'),
     Comment(id: '3', author: '익명이', content: '김콩만세!!'),
   ];
+
+  Widget buildProfileSection() {
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: SizedBox(
+            child: Image.network(
+              'https://ca.slack-edge.com/T043597JK8V-U05D6G5MADS-4ec3ce18cdf8-512',
+              width: 130,
+              height: 130,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(width: 30),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '안녕하세요.',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Front-end 개발자로 ',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              '활동하고 있는 김귀아입니다',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget buildSectionContent(String content, Color backgroundColor) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Text(
+          content,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+      width: 350,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: backgroundColor,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,138 +109,33 @@ class _KiaPageState extends State<KiaPage> {
                 const EdgeInsets.only(top: 50, left: 30, right: 30, bottom: 30),
             child: Column(
               children: [
-                Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: SizedBox(
-                        child: Image.network(
-                          'https://ca.slack-edge.com/T043597JK8V-U05D6G5MADS-4ec3ce18cdf8-512',
-                          width: 130,
-                          height: 130,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 30),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '안녕하세요.',
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          'Front-end 개발자로 ',
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          '활동하고 있는 김귀아입니다',
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                buildProfileSection(),
                 SizedBox(height: 50),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      '일본',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+                    buildSectionTitle('일본'),
                     SizedBox(height: 10),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: Text(
-                          "일본에서 사회복지를 공부했습니다",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ),
-                      width: 350,
-                      height: 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          color: Colors.white),
+                    buildSectionContent(
+                      '일본에서 사회복지를 공부했습니다',
+                      Colors.white,
                     ),
                     SizedBox(height: 25),
-                    Text(
-                      '한국',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+                    buildSectionTitle('한국'),
                     SizedBox(height: 10),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: Text(
-                          "연구원로 3년의 경험을 쌓고",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ),
-                      width: 350,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromRGBO(164, 202, 245, 1),
-                      ),
+                    buildSectionContent(
+                      '연구원로 3년의 경험을 쌓고',
+                      Color.fromRGBO(164, 202, 245, 1),
                     ),
                     SizedBox(height: 20),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: Text(
-                          "개발자로 커리어 전환을 했습니다.",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ),
-                      width: 350,
-                      height: 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          color: Color.fromRGBO(164, 202, 245, 1)),
-                      // color: Colors.white),
+                    buildSectionContent(
+                      '개발자로 커리어 전환을 했습니다.',
+                      Color.fromRGBO(164, 202, 245, 1),
                     ),
                     SizedBox(height: 20),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: Text(
-                          "여전히 배워야할게 많네요 ",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ),
-                      width: 350,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromRGBO(164, 202, 245, 1),
-                      ),
+                    buildSectionContent(
+                      '여전히 배워야할게 많네요 ',
+                      Color.fromRGBO(164, 202, 245, 1),
                     ),
                     SizedBox(height: 20),
                     Comments(
